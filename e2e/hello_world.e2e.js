8f17fb6243dbe698ed2ac921577b34b897ce90b7
Jan Werkhoven <jan.werkhoven@gmail.com>
2020-09-12 21:38:18 +1000

    Comment out E2E test until I can write better tests

--- e2e/hello_world.e2e.js
+++ e2e/hello_world.e2e.js
@@ -5,9 +5,18 @@ describe("application launch", function() {
   beforeEach(testUtils.beforeEach);
   afterEach(testUtils.afterEach);
 
-  it("shows hello world text on screen after launch", function() {
-    return this.app.client.getText("#greet").then(text => {
-      expect(text).to.equal("Hello World!");
-    });
-  });
+  // TODO: Learn about Spectron and Mocha
+  // it("renders an iframe after launch", function() {
+  //   return this.app.client
+  //     .waitUntilWindowLoaded()
+  //     .$("iframe")
+  //     .getAttribute("src")
+  //     .should.equal("http://localhost:4300");
+  // });
+
+  // it("shows hello world text on screen after launch", function() {
+  //   return this.app.client.getText("#greet").then(text => {
+  //     expect(text).to.equal("Hello World!");
+  //   });
+  // });
 });
